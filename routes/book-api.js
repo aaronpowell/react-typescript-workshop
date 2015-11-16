2c566af52cbe82d31cf2e1d22e425587b1c25d2c
Aaron Powell <me@aaron-powell.com>
2015-11-16 15:34:28 +1100

    Setting up book API for single book

--- routes/book-api.js
+++ routes/book-api.js
@@ -3,5 +3,15 @@ var store = require('./books.json');
 module.exports = {
     getAll: function (req, res) {
         res.json(store.books);
+    },
+
+    get: function (req, res) {
+        var book = store.books.filter(function (b) { return b.id === req.params.id; })[0];
+
+        if (book) {
+            res.json(book);
+        } else {
+            res.status(404);
+        }
     }
 };
